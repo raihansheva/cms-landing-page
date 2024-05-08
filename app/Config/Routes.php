0c2e75d6efcf5d55eff16eb164e2301ac33333ab
raihansheva <shevareva22@gmail.com>
2024-05-08 13:39:32 +0700

    Membuat routes untuk page fitur

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,5 +10,6 @@ $routes->get('/', 'Home::login');
 $routes->get('/dashboard', 'Home::index');
 $routes->get('/konten', 'Home::content');
 $routes->get('/solusi', 'Home::solusi');
+$routes->get('/fitur', 'Home::fitur');
 $routes->post('/tambahbanner', 'BannerController::add');
 
