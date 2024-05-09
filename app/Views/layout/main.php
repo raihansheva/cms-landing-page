fd84362e2ce295f99061488640884c4ba5157dda
raihansheva <shevareva22@gmail.com>
2024-05-09 20:56:25 +0700

    Menambahkan link untuk datatables

--- app/Views/layout/main.php
+++ app/Views/layout/main.php
@@ -6,7 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>CMS</title>
   <link rel="stylesheet" href="../assets/css/styles.min.css" />
-  <link rel="stylesheet" href="../assets/style-boostrap/css/boostrap.min.css">
+<link href="assets/bootsrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- <link rel="stylesheet" href="../assets/style-bootstrap/css/boostrap.min.css"> -->
 </head>
 
 <body id="style-3"> 
@@ -155,10 +156,10 @@
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
   <script src="../assets/js/sidebarmenu.js"></script>
-  <script src="../assets/js/app.min.js"></script>
-  <!-- <script src="../assets/libs/apexcharts/dist/apexcharts.min.js"></script> -->
-  <script src="../assets/libs/simplebar/dist/simplebar.js"></script>
-  <script src="../assets/js/dashboard.js"></script>
+  <!-- <script src="../assets/js/app.min.js"></script> -->
+  <!-- <script src="../assets/libs/apexcharts/dist/apexcharts.min.js"></script>
+  <script src="../assets/libs/simplebar/dist/simplebar.js"></script> -->
+  <!-- <script src="../assets/js/dashboard.js"></script> -->
 </body>
 
 </html>
