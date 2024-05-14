cf9dc17896847a4dd6dd4dc96be8b2684abd54a5
raihansheva <shevareva22@gmail.com>
2024-05-14 23:10:13 +0700

    Membuat ajax untuk ubah dan hapus solusi

--- public/js/ajax_solusi.js
+++ public/js/ajax_solusi.js
@@ -1,21 +1,6 @@
 
 $(document).ready(function () {
-    $(document).on('submit', '#form-data-banner', function (e) {
-        e.preventDefault();
-        // alert('oke');
-        $.ajax({
-            url: "/tambahbanner",
-            type: 'post',
-            data: $(this).serialize(),
-            dataType: "json",
-            success: function (res) {
-                if (res.status) {
-                    alert('anda berhasil menyimpan data'); 
-                }
-            }
-        })
-    })
-
+    // UNTUK TAMBAH SOLUSI
     $(document).on('submit', '#form-data-solusi', function (e) {
         e.preventDefault();
         // alert('oke');
@@ -27,6 +12,38 @@ $(document).ready(function () {
             success: function (res) {
                 if (res.status) {
                     alert('anda berhasil menyimpan data solusi'); 
+                }
+            }
+        })
+    })
+    // UNTUK UBAH SOLUSI
+    $(document).on('submit', '#form-data-ubah', function (e) {
+        e.preventDefault();
+        // alert('oke');
+        $.ajax({
+            url: "/ubahsolusi",
+            type: 'post',
+            data: $(this).serialize(),
+            dataType: "json",
+            success: function (res) {
+                if (res.status) {
+                    alert('anda berhasil mengahapus data solusi'); 
+                }
+            }
+        })
+    })
+    // UNTUK HAPUS SOLUSi
+    $(document).on('submit', '#form-data-hapus', function (e) {
+        e.preventDefault();
+        // alert('oke');
+        $.ajax({
+            url: "/hapussolusi",
+            type: 'post',
+            data: $(this).serialize(),
+            dataType: "json",
+            success: function (res) {
+                if (res.status) {
+                    alert('anda berhasil mengahapus data solusi'); 
                 }
             }
         })
