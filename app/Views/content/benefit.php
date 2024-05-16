6e9288ba0067f527873807e4d05564fcbbce8734
raihansheva <shevareva22@gmail.com>
2024-05-16 13:02:02 +0700

    Membuat page benefit

---
+++ app/Views/content/benefit.php
@@ -0,0 +1,97 @@
+<?php $this->extend('layout/main') ?>
+<?php $this->section('content') ?>
+<link rel="stylesheet" href="css/style-konten.css">
+<link rel="stylesheet" href="../assets/css/styles.min.css" />
+<link href="https://cdn.datatables.net/v/ju/dt-2.0.7/datatables.min.css" rel="stylesheet">
+<link href="assets/bootsrap/css/bootstrap.min.css" rel="stylesheet">
+<link
+    href="https://cdn.datatables.net/v/bs5/jq-3.7.0/jszip-3.10.1/dt-2.0.6/b-3.0.2/b-colvis-3.0.2/b-html5-3.0.2/b-print-3.0.2/r-3.0.2/datatables.min.css"
+    rel="stylesheet">
+
+
+<div class="bungkus">
+    <div class="konten-banner">
+        <!-- <div class="area-banner">
+    </div> -->
+        <div class="col-12 d-flex justify-content-between">
+            <div class="col-8 d-flex align-center">
+                <a href="/paketharga" class="text-dark">
+                    <i class="ti ti-chevron-left pe-4 align-middle p-1" style="font-size: 34px;"></i>
+                </a>
+                <h2>Benefit</h2>
+            </div>
+            <div class="col-4 d-flex gap-2 justify-content-end">
+                <button class="btn d-flex" type="button" data-bs-toggle="modal"
+                    data-bs-target="#exampleModaltambahfitur" style="background-color: #03C988; color:white;"><i
+                        class="ti ti-plus pe-2 fs-6 align-middle p-1 "></i>
+                    <p class="m-0 p-1 align-middle">Tambah benefit</p>
+                </button>
+            </div>
+
+
+            <!-- Modal Tambah fitur -->
+            <div class="modal fade" id="exampleModaltambahfitur" tabindex="-1" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog modal-dialog-scrollable">
+                    <div class="modal-content">
+                        <div class="modal-header border-bottom">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah benefit</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="mb-3 p-2 pt-0" style="text-align: left;">
+                                <label for="exampleFormControlInput1" class="form-label">Nama paket :</label>
+                                <input type="text" class="form-control" id="exampleFormControlInput1"
+                                    placeholder="Masukan Nama Solusi">
+                            </div>
+                            <div class="col-12 mb-3 p-2 pt-0" style="text-align: left;">
+                                <label for="exampleFormControlTextarea1" class="form-label">Benefit :</label>
+                                <textarea class="form-control" id="deskripsi" rows="8.5"
+                                    name="deskripsi"></textarea>
+                            </div>
+                        </div>
+                        <div class="modal-footer border-top pe-4">
+                            <button class="btn d-flex" type="button" style="background-color: #03C988; color:white;"><i
+                                    class="ti ti-download pe-2 fs-6 align-middle p-1 "></i>
+                                <p class="m-0 p-1 align-middle">Simpan</p>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <br>
+        <!-- <div class="card col-4" style="height: 200px; border:1px solid rgb(229, 234, 239);"> -->
+        <div class="col-12 " style="height: 100%;">
+            <table id="tabelfitur" class="table col-12 " style="height: auto;">
+                <thead class="">
+                    <tr class="p-2">
+                        <th scope="col">No</th>
+                        <th scope="col">Judul Detail</th>
+                        <th scope="col">Nama Fitur</th>
+                        <th scope="col"></th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<!-- Skrip jQuery -->
+<script src="../node_modules/jquery/dist/jquery.min.js"></script>
+<!-- Skrip DataTables -->
+<script src="../node_modules/datatables.net.jqui/js/dataTables.jqueryui.min.js"></script>
+<script src="../node_modules/datatables.net/js/dataTables.min.js"></script>
+
+<script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="js/detail-fitur.js"></script>
+<script>
+    $(document).ready(function () {
+        $('#tabelfitur').DataTable();
+    });
+</script>
+<?php $this->endsection() ?>
