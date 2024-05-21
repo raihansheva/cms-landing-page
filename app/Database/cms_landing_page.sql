d4ce542127dca04836a4dc36aba710ff8a984d5f
raihansheva <shevareva22@gmail.com>
2024-05-21 15:31:19 +0700

    database cms landing page

--- app/Database/cms_landing_page.sql
+++ app/Database/cms_landing_page.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 21, 2024 at 03:52 AM
+-- Generation Time: May 21, 2024 at 10:29 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
