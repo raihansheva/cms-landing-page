217de15f516ed0b116d7b2e603b1045dfcd92758
raihansheva <shevareva22@gmail.com>
2024-05-22 17:14:37 +0700

    Menambahkan modal untuk ubah profile dan ubah password

--- app/Views/content/profile.php
+++ app/Views/content/profile.php
@@ -81,11 +81,50 @@
                                                 class="ti ti-edit pe-2 fs-6 align-middle p-1 "></i>
                                             <p class="m-0 p-1 align-middle">Simpan perubahan</p>
                                         </button>
+                                        <button class="btn" style="background-color: #03C988; color:white; height: 45px;"
+                                            type="button" data-bs-toggle="modal"
+                                            data-bs-target="#exampleModaleditpassword"><i
+                                                class="ti ti-password pe-2 fs-5 align-middle p-1 "></i>
+                                                <span class="m-0 p-1 " style="width: 130px;">Ubah password</span></button>
                                     </div>
                                 </div>
                             </form>
                         </div>
+                        
                     </div>
+                    <div class="modal fade" id="exampleModaleditpassword" aria-hidden="true"
+                            aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+                            <div class="modal-dialog ">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Ubah Password</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                    <div class="col-12 d-flex ">
+                                            <div class="col-6 mb-3 p-2 pt-0" style="text-align: left;">
+                                                <label for="exampleFormControlInput1" class="form-label">Password lama :</label>
+                                                <input type="text" class="form-control" id="exampleFormControlInput1"
+                                                    placeholder="password lama" name="email" value="">
+                                            </div>
+                                            <div class="col-6 mb-3 p-2 pt-0" style="text-align: left;">
+                                                <label for="exampleFormControlInput1" class="form-label">Password baru :</label>
+                                                <input type="text" class="form-control" id="exampleFormControlInput1"
+                                                    placeholder="password baru" name="email" value="">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer border-top ">
+                                        <button class="btn btn-primary" data-bs-target="#exampleModaleditprofile"
+                                            data-bs-toggle="modal">Kembali</button>
+                                            <button class="btn" style="background-color: #03C988; color:white;" data-bs-target="#exampleModaleditprofile"
+                                            data-bs-toggle="modal">Ubah</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
                 </div>
             </div>
         </div>
